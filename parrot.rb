5d9710ac63f34a009124b1455aa36dfbacb59eea
Ekaterina Zarudnaya <zar.catherine@gmail.com>
2020-03-30 19:18:34 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,6 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot(sound="Pretty bird!")
-  puts "Pretty bird!"
-  "Pretty bird!"
+def parrot(phrase = "Squawk!")
+  puts "#{phrase}"
+  return phrase
 end
